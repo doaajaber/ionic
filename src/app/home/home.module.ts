429238e071541e48ae96fbe80f6a914251db5bd3
Doaa Jaber <dody_ja_1994@hotmail.com>
2019-05-06 15:42:02 +0200

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { NgProgressModule } from '@ngx-progressbar/core';
 
 import { HomePage } from './home.page';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    NgProgressModule,
     RouterModule.forChild([
       {
         path: '',
